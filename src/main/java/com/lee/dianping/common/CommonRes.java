a05935df67c3d4c09e5547a83de15c1e1c5aa9c4
lcr667742 <616716457@qq.com>
2022-08-05 14:47:22 +0800

    接入Swagger2

--- src/main/java/com/lee/dianping/common/CommonRes.java
+++ src/main/java/com/lee/dianping/common/CommonRes.java
@@ -1,17 +1,22 @@
 package com.lee.dianping.common;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 /**
  * @Description 公共请求返回对象
  * @Author 61671
  * @Date 2022/7/27 23:26
  * @Version 1.0.0
  */
+@ApiModel(value = "返回对象", description = "返回对象")
 public class CommonRes {
 
     /**
      * 请求返回处理结果
      * 成功：success 失败：fail
      */
+    @ApiModelProperty(value = "请求返回处理结果", name = "status", example = "success")
     private String status;
 
     /**
@@ -19,6 +24,7 @@ public class CommonRes {
      * 1、若status=success时，表明对应的返回的json类数据
      * 2、若status=fail时，则data内将使用通用的错误码对应的格式
      */
+    @ApiModelProperty(value = "返回结果对象", name = "data")
     private Object data;
 
     /**
--- src/main/java/com/lee/dianping/controller/UserController.java
+++ src/main/java/com/lee/dianping/controller/UserController.java
@@ -5,6 +5,9 @@ import com.lee.dianping.common.CommonRes;
 import com.lee.dianping.common.EmBusinessError;
 import com.lee.dianping.entity.User;
 import com.lee.dianping.service.IUserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,6 +23,7 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @RestController
 @RequestMapping("/user")
+@Api(tags = "用户入口")
 public class UserController {
 
     @Autowired
@@ -32,6 +36,8 @@ public class UserController {
      * @description 根据用户id获取用户
      * @date 2022/7/24 11:08
      */
+    @ApiOperation(value = "根据用户id获取用户", response = CommonRes.class, httpMethod = "GET")
+    @ApiImplicitParam(name = "id", value = "用户id", required = true)
     @RequestMapping("/get")
     @ResponseBody
     public CommonRes getUser(@RequestParam(name = "id") int id) throws BusinessException {
@@ -51,6 +57,8 @@ public class UserController {
      * @description TODO
      * @date 2022/7/23 22:57
      */
+    @ApiOperation(value = "用户测试接口", httpMethod = "GET")
+    @ApiImplicitParam
     @RequestMapping("/test")
     @ResponseBody
     public ModelAndView test() {
