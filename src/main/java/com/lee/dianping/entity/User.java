d076f67fc0451df87b356ee442badb189027ecbc
lcr667742 <616716457@qq.com>
2022-08-05 17:50:47 +0800

    修改注释

--- src/main/java/com/lee/dianping/entity/User.java
+++ src/main/java/com/lee/dianping/entity/User.java
@@ -4,16 +4,14 @@ import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.Date;
 
 /**
- * <p>
- * 
- * </p>
- *
+ * 用户
  * @author 61671
  * @since 2022-08-05
  */
@@ -46,9 +44,11 @@ public class User implements Serializable {
 
 
     @TableField(fill = FieldFill.INSERT)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone ="GMT+8")
     private Date createdAt;
 
     @TableField(fill = FieldFill.INSERT_UPDATE)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone ="GMT+8")
     private Date updatedAt;
 
     public Integer getId() {
