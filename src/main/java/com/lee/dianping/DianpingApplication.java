182de979f152e9116b57f6a71fc1022f4a18ac8a
lcr667742 <616716457@qq.com>
2022-08-31 16:52:52 +0800

    接入SpringSession

--- src/main/java/com/lee/dianping/DianpingApplication.java
+++ src/main/java/com/lee/dianping/DianpingApplication.java
@@ -3,9 +3,11 @@ package com.lee.dianping;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication(scanBasePackages = {"com.lee.dianping"})
 @MapperScan("com.lee.dianping.mapper")
+@EnableRedisHttpSession
 public class DianpingApplication {
 
     public static void main(String[] args) {
